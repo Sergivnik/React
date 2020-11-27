88fcc5c54ebe4f3e03a48a4644444416ae47e433
Serg <sergivnik@mail.ru>
2020-11-27 14:30:03 +0300

    v-1.1

--- src/index.js
+++ src/index.js
@@ -1,43 +1,38 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import { Counter } from "./components/Counter.jsx";
 
-const messeges = [
-  { name: "Федор", content: "Hello, World!" },
-  { name: "World", content: "Hello, Федор! How are you?" },
-  { name: "Федор", content: "Fine, thanks!" },
-];
+// const messeges = [
+//   { name: "Федор", content: "Hello, World!" },
+//   { name: "World", content: "Hello, Федор! How are you?" },
+//   { name: "Федор", content: "Fine, thanks!" },
+// ];
 
-const Messege = ({ name, content }) => {
-  return (
-    <li>
-      <strong>{name}: </strong>
-      {content}
-    </li>
-  );
-};
+// const Messege = ({ name, content }) => {
+//   return (
+//     <li>
+//       <strong>{name}: </strong>
+//       {content}
+//     </li>
+//   );
+// };
 
-const MessegeList = ({ messeges }) => {
-  function btnPush() {
-    messeges.push({ name: "Кнопка", content: "Satisfaction!!!" });
-    rend();
-  }
-  return (
-    <div>
-      <button onClick={btnPush}>Push me!!!</button>
-      <ul>
-        {messeges.map((item, index) => (
-          <Messege {...item} key={index} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+// const MessegeList = ({ messeges }) => {
+//   function btnPush() {
+//     messeges.push({ name: "Кнопка", content: "Satisfaction!!!" });
+//     rend();
+//   }
+//   return (
+//     <div>
+//       <button onClick={btnPush}>Push me!!!</button>
+//       <ul>
+//         {messeges.map((item, index) => (
+//           <Messege {...item} key={index} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-function rend() {
-  ReactDOM.render(
-    <MessegeList messeges={messeges} />,
-    document.getElementById("root")
-  );
-}
-rend();
+ReactDOM.render(<Counter />, document.getElementById("root"));
