d00718f8db857d0177cb612eafdc3463270c699b
Serg <sergivnik@mail.ru>
2020-11-29 15:27:41 +0300

    v-3.1 metodichka

--- src/components/MessageField/MessageField.jsx
+++ src/components/MessageField/MessageField.jsx
@@ -1,36 +1,70 @@
-import React from 'react';
-import Message from '../message/Message';
-import '../MessageField/style.css';
+import React from "react";
+import Message from "../message/Message";
+import "./style.css";
 
 export default class MessageField extends React.Component {
-   state = {
-       messages: [
-           {name:"Федор", text: "Привет!"}
-            ]
-   };
-   componentDidUpdate() {
-       if (this.state.messages[this.state.messages.length-1].name!="Робот") {
-           setTimeout(() =>
-           this.setState(
-               { messages: [ ...this.state.messages, {name:"Робот", text:'Не приставай ко мне, я робот!'} ] }),
-           1000);
-       }
-   }
+  state = {
+    messages: [
+      { sender: "Робот", text: "Привет!" },
+      { sender: "Робот", text: "Как дела?" },
+    ],
+    input: "",
+  };
+  componentDidUpdate() {
+    if (this.state.messages[this.state.messages.length - 1].sender != "Робот") {
+      setTimeout(
+        () =>
+          this.setState({
+            messages: [
+              ...this.state.messages,
+              { sender: "Робот", text: "Не приставай ко мне, я робот!" },
+            ],
+          }),
+        1000
+      );
+    }
+  }
 
+  handleClick = (message) => {
+    this.sendMessage(message);
+  };
 
-   handleClick = () => {
-       this.setState({ messages: [ ...this.state.messages, {name:"Федор", text: "Как дела?"} ] });
-   };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-   render() {
-       const messageElements = this.state.messages.map(function (text, index){
-           console.log(index);
-           return (<Message key={ index } text={ text } />)});
-        
-       return <div id="main" className="message-field">
-           { messageElements }
-            <p>{this.state.messages.length}</p>
-           <button onClick={ this.handleClick }>Отправить сообщение</button>
-       </div>
-   }
+  handleKeyUp = (event, message) => {
+    if (event.keyCode === 13) {
+      // Enter
+      this.sendMessage(message);
+    }
+  };
+
+  sendMessage = (message) => {
+    this.setState({
+      messages: [...this.state.messages, { text: message, sender: "me" }],
+      input: "",
+    });
+  };
+
+  render() {
+    const messageElements = this.state.messages.map(function (item, index) {
+      return <Message key={index} text={item} />;
+    });
+    return (
+      <div className="layout">
+        <div className="message-field">{messageElements}</div>
+        <p>{this.state.messages.length}</p>
+        <input
+          name="input"
+          onChange={this.handleChange}
+          value={this.state.input}
+          onKeyUp={(event) => this.handleKeyUp(event, this.state.input)}
+        />
+        <button onClick={() => this.handleClick(this.state.input)}>
+          Отправить сообщение
+        </button>
+      </div>
+    );
+  }
 }
